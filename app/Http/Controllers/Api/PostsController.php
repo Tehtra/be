270892840985b6437baf6acf5bafe60ae5b8a7d8
Tehtra <finternp.11@gmail.com>
2022-09-20 19:30:12 +0700

    initial commit

---
+++ app/Http/Controllers/Api/PostsController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostsRequest;
+use App\Http\Resources\PostsResource;
+use Validator;
+use App\Models\Posts;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Posts::all();
+        return PostsResource::collection($posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = $request->validate([
+            'title' => 'required|min:20',
+            'content' => 'required|min:200',
+            'category' => 'required|min:3',
+            'status' => 'required|in:publish,draft,thrash'
+        ]);
+
+        $posts = Posts::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'category' => $request->category,
+            'status' => $request->status
+         ]);
+        
+        return response()->json(['Post created successfully.', new PostsResource($posts)]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Posts  $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Posts  $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Posts  $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Posts $posts)
+    {
+        $validator = $request->validate([
+            'title' => 'required|min:20',
+            'content' => 'required|min:200',
+            'category' => 'required|min:3',
+            'status' => 'required|in:publish,draft,thrash'
+        ]);
+
+        $posts->title = $request->title;
+        $posts->content = $request->content;
+        $posts->category = $request->category;
+        $posts->status = $request->status;
+        $posts->save();
+        
+        return response()->json(['Post updated successfully.', new PostsResource($posts)]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Posts  $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Posts $posts)
+    {
+        $posts->delete();
+
+        return response()->json('Post deleted successfully');
+    }
+}
---
+++ app/Http/Requests/StorePostsRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'min:2'],
+            'content'  => ['required','min:2'],
+            'category'  => ['required','min:3'],
+            'status'  => ['required','in:publish,draft,thrash']
+        ];
+    }
+}
